05493f6d81308955cdff8607098a71ca179037bf
nut <nut999anan@hotmail.com>
2024-09-23 18:26:19 +0700

    update providers

--- app/services/transcribe_processor.py
+++ app/services/transcribe_processor.py
@@ -9,13 +9,31 @@ class TranscribeProcessor:
     
     async def process(self, request_data):
         audio_file = request_data.get_audio_file()
+        provider_name = request_data.get_stt_provider()
+
+        if provider_name == 'google':
+            return await self.google_transcribe(audio_file)
+        elif provider_name == 'azure':
+            return await self.azure_transcribe(audio_file)
+        elif provider_name == 'aws':
+            return await self.aws_transcribe(audio_file)
+        else:
+            return await self.openai_transcribe(audio_file)
+  
+
+    async def openai_transcribe(self, audio_file):
         audio_file_path = files_handler.save_audio_file(audio_file)
         try:
-            response = await self.openai_transcribe(audio_file_path)
-            return response
+            return await self.openai_provider.transcribe_audio_file(audio_file_path)
         finally:
             files_handler.delete_file(audio_file_path)
+    
+    async def google_transcribe(self, audio_file_path):
+        pass
 
-    async def openai_transcribe(self, audio_file_path):
-        return await self.openai_provider.transcribe_audio_file(audio_file_path)
+    async def azure_transcribe(self, audio_file_path):
+        pass
+
+    async def aws_transcribe(self, audio_file_path):
+        pass
     
