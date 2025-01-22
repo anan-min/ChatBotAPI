55df29dc0a1ad7fc402f072771e1e590d303ed1f
nut <nut999anan@hotmail.com>
2025-01-23 00:01:48 +0700

    update requirement and etc

--- app/providers/google_provider.py
+++ app/providers/google_provider.py
@@ -1,11 +1,11 @@
 import os
 from pathlib import Path
-from pydub import AudioSegment
 from google.cloud import texttospeech, speech_v1 as speech
 from google.cloud import language_v1
 from app.configs.google_config import api_credentials, voice_configs, audio_configs, transcribe_configs
-from scipy.io import wavfile
-from scipy.signal import resample
+import soundfile as sf  
+import audioread
+import scipy.signal 
 
 class GoogleProvider:
     def __init__(self) -> None:
@@ -51,54 +51,45 @@ class GoogleProvider:
     
     # Return the audio content for further processing
         return response.audio_content
-
-    # Convert audio sample rate method
-    def convert_audio_sample_rate(self, input_file_path, target_sample_rate=16000):
-        input_file_path = str(input_file_path)
-        # Determine file type and load the audio
-        if input_file_path.endswith('.mp3'):
-            audio = AudioSegment.from_mp3(input_file_path)
-        elif input_file_path.endswith('.wav'):
-            audio = AudioSegment.from_wav(input_file_path)
-        else:
-            raise ValueError("Unsupported file format. Please use MP3 or WAV files.")
-
-        # Set the target sample rate and channels
-        audio = audio.set_frame_rate(target_sample_rate).set_channels(1)
-
-        # Export the audio back to the same path (overwrite)
-        output_file = input_file_path  # Overwrite the input file
-        audio.export(output_file, format="wav")
-        
-        return output_file
     
-    def convert_audio_sample_rate_2(self, input_file_path, target_sample_rate=16000):
+
+    def generate_audio_output_file_path(input_file_path, output_file_path):
         input_file_path = Path(input_file_path)
-        input_file_path = str(input_file_path)   
-        output_file_path = self.generate_output_file_path(input_file_path)
+            
+        temp_folder = input_file_path.parent / "temp"
+        temp_folder.mkdir(exist_ok=True)
 
-        sample_rate, data = wavfile.read(input_file_path)
+        if output_file_path is None:
+            output_file_path = temp_folder /  input_file_path.with_suffix('.wav').name
 
-        # Resample only if the sample rate is different
-        if sample_rate != target_sample_rate:
-            num_samples = int(len(data) * target_sample_rate / sample_rate)
-            data = resample(data, num_samples).astype(data.dtype)
+        return str(output_file_path)
+    
 
-        # Write the resampled audio to the output file
-        wavfile.write(output_file_path, target_sample_rate, data)
+    def convert_audio_sample_rate(self, input_file_path, target_sample_rate=16000):
+        output_file_path = self.generate_audio_output_file_path(input_file_path)
 
+        data, original_sameple_rate = sf.read(input_file_path)
+
+        if input_file_path.suffix != ".wav":
+            input_file_path = self.convert_to_wav(input_file_path)
+
+        if original_sameple_rate != target_sample_rate:
+            num_samples = int(len(data) * target_sample_rate / original_sameple_rate)
+            data = scipy.signal.resample(data, num_samples).astype(data.dtype)
+
+            sf.write(output_file_path, data, target_sample_rate, format='WAV')
+
+        return str(output_file_path)
+    
+    def convert_to_wav(self, input_file_path):
+
+        output_file_path = input_file_path.with_suffix('.wav').name
+
+        with audioread.audio_open(input_file_path) as input_file:
+            data = b"".join(input_file.read_data() for _ in input_file)
+            sample_rate = input_file.samplerate
+
+        sf.write(output_file_path, sf.read(data, dtype='int16')[0], sample_rate, format='WAV', subtype='PCM_16')
+        
         return output_file_path
-
-
-
-    def generate_output_file_path(self, input_file_path):
-        input_file_path = str(input_file_path)
-        input_dir = Path(input_file_path).parent
-
-        # Create an "output" folder within the input file's directory
-        output_dir = input_dir / "output"
-        output_dir.mkdir(exist_ok=True)  # Create the folder if it doesn't exist
-
-        # Set the output file path in the "output" folder
-        output_file_path = output_dir / f"{Path(input_file_path).stem}_converted.wav"
-        return output_file_path
+            
